f2bf6c9eb423a6048b634cc62eefc2846fbd7ba3
brleunga <brleung@s.sfusd.edu>
2018-10-15 08:38:07 -0700

    added animal classes

--- Farm.java
+++ Farm.java
@@ -1,4 +1,14 @@
 class Farm 
 {     
-   //your code here
+   private Animal[] aBunchOfAnimals = new Animal[3];
+   public Farm() {
+   	aBunchofAnimals[0] = new Cow("cow", "moo");
+   	aBunchofAnimals[1] = new Pig("pig", "oink");
+   	aBunchofAnimals[2] = new Chick("chick", "cluck");
+   }
+   public void animalSounds() {
+   	for (int i = 0; i < aBunchofAnimals.length; i++) {
+   		System.out.println(aBunchofAnimals.getType + " goes " + aBunchofAnimals.getSound);
+   	}
+   }
 }
